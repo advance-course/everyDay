b4ecbb3de7b58cade170b995046a9dd6bdd8da8e
yao <648989873@qq.com>
2020-12-08 16:02:14 +0800

    frist commit

---
+++ src/components/element/Icon/map.js
@@ -0,0 +1,10 @@
+export default {
+  "tongzhi": '\ue602',
+  "feather-blur-light": "\ue6f7",
+  "jiazai": "\ue622",
+  "xin": "\ue750",
+  "liaotian-kuang": "\ue628",
+  "zuoyoujiantou": "\ue600",
+  "aA": "\ue623",
+  "left": "\ue601",
+}
